4c07398c6c7d25f4723cfea4260462702a0ddf70
Heru Azy <heruazyp@gmail.com>
2024-07-25 11:08:23 +0700

    Initial Project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['*.html'],
+	theme: {
+		fontFamily: {
+			figtree: ['Figtree']
+		},
+		container: {
+			center: true
+		},
+		extend: {
+			colors: {
+				primary: '#F6D050',
+				secondary: '#111111'
+			}
+		}
+	},
+	plugins: []
+}
